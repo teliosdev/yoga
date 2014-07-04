419c7dcdb135ad25b7c7f070fd0b4747281ed31f
Jeremy Rodi <redjazz96@gmail.com>
2014-07-04 16:06:22 -0400

    Testing nondeterminism

--- test.rb
+++ test.rb
@@ -6,8 +6,8 @@ require "terminal-table"
 #exp = Yoga::Expression.new(%q{(('0x' [0-9a-fA-F]+) | ([0-9]+) | ([A-Za-z][A-Za-z0-9]*))})
 #exp = Yoga::Expression.new(%q{([^bcd]* ('b' |('c' 'd'?))+)})
 #exp = Yoga::Expression.new(%q{([a-z]+ - ('for' | 'int') :>$<1>)})
-exp = Yoga::Expression.new(%q{any* "\r\n" any*})
-#exp = Yoga::Expression.new(%q{":" ((any*) - (any* "\r\n" any* :<$test<1>)) "end"})
+#exp = Yoga::Expression.new(%q{any* "\r\n" any*})
+exp = Yoga::Expression.new(%q{":" ((any*) - (any* "\r\n" any* :<$test<1>)) "end"})
 
 machine = exp.build!
 machine.determinitize!
